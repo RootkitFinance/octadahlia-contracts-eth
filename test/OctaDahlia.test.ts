7da66b6dd00e25d8a641b89c2da898871299dbc6
bashmarkz <>
2021-10-08 18:20:13 +0300

    Setup OctaDahlia tests 1/2

---
+++ test/OctaDahlia.test.ts
@@ -0,0 +1,57 @@
+import { ethers } from "hardhat";
+import { utils, constants, Contract } from "ethers";
+import { expect } from "chai";
+import { parseEther } from "ethers/lib/utils";
+
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
+import { ContractFunction } from "hardhat/internal/hardhat-network/stack-traces/model";
+
+import { OctaDahlia } from "../typechain/OctaDahlia";
+import { createUniswap } from './helpers'
+
+describe.only("OctaDahlia", async function () {
+
+    let owner: SignerWithAddress, user1: SignerWithAddress, user2: SignerWithAddress, user3: SignerWithAddress;
+    let octaDahlia: OctaDahlia & Contract;
+    let paired: Contract;
+    let uniswap: { router: Contract; factory: Contract; };
+
+    beforeEach(async function () {
+        [owner, user1, user2, user3] = await ethers.getSigners();
+        const octaDahliaFactory = await ethers.getContractFactory("OctaDahlia");
+        octaDahlia = (await octaDahliaFactory.connect(owner).deploy()) as any as OctaDahlia & Contract
+
+        uniswap = await createUniswap(owner);
+        const pairedFactory = await ethers.getContractFactory("ERC20Test");
+        paired = await pairedFactory.connect(owner).deploy();
+
+        await paired.connect(owner).approve(octaDahlia.address, constants.MaxUint256);
+        await paired.connect(owner).approve(uniswap.router.address, constants.MaxUint256);
+    })
+
+    describe('setUp(IUniswapV2Pair _pair, address dev6, address dev9, address _mge, bool _dictator)', function () {
+        it('should only initialize once', async function () {
+            let mge_address = user3.address
+
+            // await expect(octaDahlia.connect(owner).setUp(uniswap.factory.address, user1.address, user2.address, mge_address, false)).to.not.be.reverted
+            // await expect(octaDahlia.connect(owner).setUp(uniswap.router.address, user1.address, user2.address, mge_address, false)).to.be.reverted
+        })
+        it('should only allow pair that includes correct token')
+    })
+    describe('balanceAdjustment(bool increase, uint256 _amount, address _account)', function () {
+
+    })
+
+    describe('alignPrices()', function () { })
+
+    describe('recoverTokens(IERC20 token)', function () { })
+    describe('addOrChangeFriends(uint256 indexSpot, address friend)', function () { })
+
+
+    describe('_transfer(address sender, address recipient, uint256 amount) internal', function () { })
+    describe('_burnAndFees(address account, uint256 amount, uint256 burnPercent) internal', function () { })
+    describe('dynamicBurnRate() internal', function () { })
+    describe('getAmountOut(uint amountIn) internal', function () { })
+
+
+})
--- test/helpers.ts
+++ test/helpers.ts
@@ -6,10 +6,11 @@ import { utils } from "ethers";
 import UniswapV2FactoryJson from '../contracts/json/UniswapV2Factory.json';
 import UniswapV2Router02Json from '../contracts/json/UniswapV2Router02.json';
 
-exports.createUniswap = async function(owner: SignerWithAddress) {
+export async function createUniswap(owner: SignerWithAddress) {
     const erc20Factory = await ethers.getContractFactory("ERC20Test");
     const weth = await erc20Factory.connect(owner).deploy();
     const factory = await new ethers.ContractFactory(UniswapV2FactoryJson.abi, UniswapV2FactoryJson.bytecode, owner).deploy(owner.address);
     const router = await new ethers.ContractFactory(UniswapV2Router02Json.abi, UniswapV2Router02Json.bytecode, owner).deploy(factory.address, weth.address);
     return { factory, router };
 }
+
